c8d0bab39e82dbb96445869de7753fc5d2d6da25
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-26 08:43:07 +0300

    Create objectpriority.js

---
+++ objectpriority.js
@@ -0,0 +1,12 @@
+//instead of doing this
+let name = "John";
+let age = 25;
+let person ={
+  name : name,
+  age : age
+};
+
+//the do this
+let name = "John";
+let age = 25;
+let person ={ name , age};
